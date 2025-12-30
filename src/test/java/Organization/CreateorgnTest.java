0179e0874d2eed509f33f1f3c5f013ddfde88279
Shanthi M <Shanthi M@LAPTOP-D7GO7M62>
2025-12-30 18:06:45 +0530

    edited

--- src/test/java/Organization/CreateorgnTest.java
+++ src/test/java/Organization/CreateorgnTest.java
@@ -1,8 +1,12 @@
 package Organization;
 
+import org.testng.annotations.Test;
+
 public class CreateorgnTest 
 {
-	public static void main(String[] args) {
+	@Test
+	public void m2()
+	{
 		System.out.println("orgn");
 	}
 
